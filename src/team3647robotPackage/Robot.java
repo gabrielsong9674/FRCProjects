d859d46f148c192bbaacd1a4f4ea2552c452c63c
gabrielsong9674 <gracegabrielson@icloud.com>
2017-12-19 16:00:02 -0800

    update

--- src/team3647robotPackage/Robot.java
+++ src/team3647robotPackage/Robot.java
@@ -1,71 +1,86 @@
 package team3647robotPackage;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class Robot extends IterativeRobot 
-{	
-	//The speed of the robot while running the program
-	double speed = 0.3;
-	double distance = 5;
-	double wheelcirc = 1;
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+	// The speed of the robot while running the program
+	double leftSpeed = 0.4;
+	double rightSpeed = 0.4;
+	double distance = 1400;
+	double adjustment = 0.005;
+	double leftEncoderValue;
+	double rightEncoderValue;
+	double speed = .3;
 
-	//This function is run whenever the robot starts. This function is used for any initialization of code
+	// This function is run whenever the robot starts. This function is used for any
+	// initialization of code
 
 	Encoders encoderObject;
-	public void robotInit() 
-	{
-encoderObject = new Encoders();
+
+	public void robotInit() {
+		encoderObject = new Encoders();
 	}
 
-	 //This function runs once, right before autonomous period starts. 
+	// This function runs once, right before autonomous period starts.
 
-	public void autonomousInit() 
-	{
-	encoderObject.resetEncoders();
+	public void autonomousInit() {
+		encoderObject.resetEncoders();
 	}
 
-	//This is the function that is called during the autonomous period
-	//This function runs periodically, meaning it acts as an infinite loop
-	
-	public void autonomousPeriodic() 
-	{
-		double leftEncoderValue = encoderObject.getLeftEncoder();
-		double rightEncoderValue = encoderObject.getRightEncoder();
+	// This is the function that is called during the autonomous period
+	// This function runs periodically, meaning it acts as an infinite loop
+
+	public void autonomousPeriodic() {
+
 		goStraight(distance);
 	}
+
 	public void goStraight(double distance) {
-		if(encoderObject.getLeftEncoder() *wheelcirc<distance) {
-			Motors.leftMotor.set(speed);
-			
+		leftEncoderValue = encoderObject.getLeftEncoder();
+		rightEncoderValue = encoderObject.getRightEncoder();
+		if (Math.abs(rightEncoderValue - leftEncoderValue) < 5) {
+			rightSpeed = .4;
+			leftSpeed = .4;
+		} else {
+			if (rightEncoderValue > leftEncoderValue) {
+				rightSpeed -= adjustment;
+				leftSpeed += adjustment;
+			} else {
+				rightSpeed += adjustment;
+				leftSpeed -= adjustment;
+			}
 		}
-		else
-		{
+		if (rightEncoderValue / 2 + leftEncoderValue / 2 < distance) {
+			Motors.leftMotor.set(leftSpeed);
+			Motors.rightMotor.set(-rightSpeed);
+		} else {
 			Motors.leftMotor.set(0);
-		}
-		if(encoderObject.getRightEncoder()*wheelcirc<-distance) {
-			Motors.rightMotor.set(speed);
-			
-		}
-		else {
 			Motors.rightMotor.set(0);
 		}
 	}
 
-	//This is the function that is called during the Tele-operated period
-	//This function runs periodically, meaning it acts as an infinite loop
+	// This is the function that is called during the Tele-operated period
+	// This function runs periodically, meaning it acts as an infinite loop
 
-	public void teleopPeriodic() 
-	{
-		
+	public void teleopPeriodic() {
+
 	}
 
-	//This is the function that is called during the test
-	//Test is an option available in the driver station and can be used to test specific pieces of code.
-	//This function runs periodically, meaning it acts like an infinite loop
-	
-	public void testPeriodic() 
-	{
-		
+	// This is the function that is called during the test
+	// Test is an option available in the driver station and can be used to test
+	// specific pieces of code.
+	// This function runs periodically, meaning it acts like an infinite loop
+
+	public void testPeriodic() {
+
 	}
 }
-
